a4bdfdc197238a87cd11ec8bbd7b3b29bd092938
Oliver Brown <oliver.brown@chelsea-apps.com>
2016-09-29 10:10:49 +0100

    Initial commit.

---
+++ FrameColorDemo/FrameColorDemoPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace FrameColorDemo
+{
+    public partial class FrameColorDemoPage : ContentPage
+    {
+        public FrameColorDemoPage()
+        {
+            InitializeComponent();
+            BindingContext = new List<Color>
+            { 
+                Color.Aqua, 
+                Color.Black, 
+                Color.Blue, 
+                Color.Fuchsia, 
+                Color.Gray, 
+                Color.Green, 
+                Color.Lime, 
+                Color.Maroon, 
+                Color.Navy, 
+                Color.Olive, 
+                Color.Pink, 
+                Color.Purple, 
+                Color.Red, 
+                Color.Silver, 
+                Color.Teal, 
+                Color.White, 
+                Color.Yellow };
+        }
+    }
+}
